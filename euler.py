bc1c1b3423577138ab498efd595b9840862647bc
David Moody <david@davidxmoody.com>
2012-04-10 16:18:13 +0100

    Added working but slow euler10.py

--- euler.py
+++ euler.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 
+from math import sqrt
+
 def primes_below(num=1000000000):
     primes = [2]
     yield 2
     for i in range(3, num, 2):
         if not any((i%p==0 for p in primes)):
+            primes.append(i)
+            yield i
+
+
+def primes_below_efficient(num=1000000000):
+    primes = [2]
+    yield 2
+    for i in range(3, num, 2):
+        divisible = False
+        sqrti = sqrt(i)
+        for p in primes:
+            if p > sqrti: continue
+            if i%p==0: 
+                divisible = True
+                break
+        if not divisible:
             primes.append(i)
             yield i
