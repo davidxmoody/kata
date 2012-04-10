e8e54fbc87d657a79a4640674682a1077c1134f3
David Moody <david@davidxmoody.com>
2012-04-10 10:48:19 +0100

    Made primes_below much quicker and gave it a default argument

--- euler.py
+++ euler.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-def primes_below(num):
-    primes = []
-    for i in range(2, num):
-        if not any([i%p==0 for p in primes]):
+def primes_below(num=1000000000):
+    primes = [2]
+    yield 2
+    for i in range(3, num, 2):
+        if not any((i%p==0 for p in primes)):
             primes.append(i)
             yield i
