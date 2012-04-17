3f58fde3195a6ec7f7f2894ac51830f9ce86501e
David Moody <david@davidxmoody.com>
2012-04-17 17:53:10 +0100

    Added working euler12.py

--- euler12.py
+++ euler12.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-# tri(n) = SUM from 1 to n of (n) = n/2(n+1)
-# Need to find smallest n/2(n+1) which has at least 500 divisors
+import math
 
 def get_triangles():
     i = 2
@@ -11,12 +10,16 @@ def get_triangles():
         tri += i
         i += 1
 
+def num_factors(n):
+    # Note that this will be off by one for 1 or for a square number.
+    result = 0
+    for i in range(1, math.floor(math.sqrt(n))+1):
+        if n%i==0: 
+            result += 2
+    return result
+
+
 for tri in get_triangles():
-    num_factors = 1
-    for i in range(1, tri//2):
-        if tri%i==0:
-            num_factors += 1
-    print(tri, num_factors)
-    if num_factors > 500:
+    if num_factors(tri) > 500:
         print(tri)
         break
