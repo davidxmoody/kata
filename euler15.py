f7363150e785171e4ec8132240ac99284a3f0032
David Moody <david@davidxmoody.com>
2012-04-17 08:45:35 +0100

    Added working euler15.py

--- euler15.py
+++ euler15.py
@@ -1,22 +1,6 @@
 #!/usr/bin/env python3
 
-def step(state):
-    x, y = state
-    if x < 20:
-        yield (x+1, y)
-    if y < 20:
-        yield (x, y+1)
+from math import factorial
 
-
-paths_found = 0
-states = [(0, 0)]
-
-while len(states) > 0:
-    print(len(states), paths_found)
-    state = states.pop()
-    if state == (20, 20):
-        paths_found += 1
-    else:
-        states.extend(step(state))
-
-print(paths_found)
+n = 20
+print(factorial(n*2)/factorial(n)/factorial(n))
